75e1879fe97298a9d40370ff23dc86e4ac506246
amirhdre <daraieamirh@gmail.com>
2021-12-30 01:26:49 +0330

    s3.m updated with new DTFT codes for property validation

--- s3.m
+++ s3.m
@@ -194,3 +194,151 @@ X1 = x1*(exp(-1i*pi/M)).^(n'*k);
 XX1 = conj(sigfold(X,w));
 subplot(3,2,5); hold on; plot(k/M, abs(X1),'k'); plot(k/M, abs(XX1),'r--','linewidth',2);
 subplot(3,2,6); hold on; plot(k/M, angle(X1)/pi,'k'); plot(k/M, angle(XX1)/pi,'r--','linewidth',2); ylim([-1.5;1.5]);
+
+%% folding  
+
+nx = -1:3;
+x = randn(1,5);
+[x1,nx1] = sigfold(x,nx);
+
+M = 500;
+k = -M:M;
+w = (pi/M)*k;
+
+X = x*(exp(-1i*pi/M)).^(nx'*k);
+X1 = x1*(exp(-1i*pi/M)).^(nx1'*k);
+
+subplot(4,1,1)
+stem(nx,x); xlim([-5 5])
+
+subplot(4,1,2)
+stem(nx1,x1); xlim([-5 5])
+
+subplot(4,1,3); hold on;
+plot(w/pi, real(X), 'k')
+plot(w/pi, real(X1), 'r--')
+
+subplot(4,1,4); hold on;
+plot(w/pi, imag(X), 'k')
+plot(w/pi, imag(X1), 'r--')
+
+%% convolution 
+
+nx1 = -1:3;
+x1 = randn([1 5]);
+nx2 = -3:2;
+x2 = randn([1 6]);
+[x3, nx3] = conv_m(x1,nx1,x2,nx2);
+
+M = 500;
+k = -M:M;
+w = (pi/M)*k;
+
+X1 = x1*(exp(-1i*pi/M)).^(nx1'*k);
+X2 = x2*(exp(-1i*pi/M)).^(nx2'*k);
+X3 = x3*(exp(-1i*pi/M)).^(nx3'*k);
+XX3 = X1.*X2;
+
+figure
+subplot(5,1,1); stem(nx1,x1); xlim([-6 6]);
+subplot(5,1,2); stem(nx2,x2); xlim([-6 6]);
+subplot(5,1,3); stem(nx3,x3); xlim([-6,6]);
+
+subplot(5,1,4); hold on;
+plot(w/pi, real(X3), 'k')
+plot(w/pi, real(XX3), 'r--')
+
+subplot(5,1,5); hold on;
+plot(w/pi, imag(X3), 'k')
+plot(w/pi, imag(XX3), 'r--')
+
+%% correlation
+
+nx1 = -1:3;
+x1 = randn([1 5]);
+nx2 = -3:2;
+x2 = randn([1 6]);
+[x3, nx3] = xcorr_m(x1,nx1,x2,nx2);
+
+% define frequency axis
+M = 500;
+k = -M:M;
+w = (pi/M)*k;
+
+X1 = x1*(exp(-1i*pi/M)).^(nx1'*k);
+X2 = x2*(exp(-1i*pi/M)).^(nx2'*k);
+X3 = x3*(exp(-1i*pi/M)).^(nx3'*k);
+XX3 = X1.*conj(X2);
+
+figure;
+subplot(5,1,1); stem(nx1,x1); xlim([-6 6]);
+subplot(5,1,2); stem(nx2,x2); xlim([-6 6]);
+subplot(5,1,3); stem(nx3,x3); xlim([-6 6]);
+ 
+subplot(5,1,4); hold on;
+plot(w/pi, real(X3), 'k');
+plot(w/pi, real(XX3), 'r--');
+
+subplot(5,1,5); hold on;
+plot(w/pi, imag(X3), 'k');
+plot(w/pi, imag(XX3), 'r--');
+
+%% multiplication
+
+nx1 = -1:3;
+x1 = randn([1 5]);
+nx2 = -3:2;
+x2 = randn([1 6]);
+[x3, nx3] = sigmult(x1,nx1,x2,nx2);
+
+% define frequency axis
+M = 500;
+k = -M:M;
+w = (pi/M)*k;
+
+X1 = x1*(exp(-1i*pi/M)).^(nx1'*k);
+X2 = x2*(exp(-1i*pi/M)).^(nx2'*k);
+X3 = x3*(exp(-1i*pi/M)).^(nx3'*k);
+[XX3,wp] = conv_m(X1,k,X2,k);
+
+figure;
+subplot(5,1,1); stem(nx1,x1); xlim([-6 6]);
+subplot(5,1,2); stem(nx2,x2); xlim([-6 6]);
+subplot(5,1,3); stem(nx3,x3); xlim([-6 6]);
+ 
+subplot(5,1,4); hold on;
+plot(w/pi, abs(X3), 'k');
+
+subplot(5,1,5); hold on;
+plot(wp/1000, abs(XX3), 'k');
+
+% Question: why the 4th and 5th plots are different? Why the 5th plot has
+% a larger range than the 4th plot?
+
+%% Parseval's Theorem (energy)
+
+nx = -1:3;
+x = randn([1,5]);
+
+E1 = sum(x.^2);
+
+M = 100;
+k = -M:M;
+w = (pi/M)*k;
+X = x*(exp(-1i*pi/M)).^(nx'*k);
+
+E2 = trapz(abs(X).^2)*(pi/M)/(2*pi);
+% we multiply the trapz integral result with (pi/M), because trapz computes 
+% an approximation of the integral of Y via the trapezoidal method (with unit spacing)
+% thus, to create dw with pi/M spacing, we multiply it by pi/M.
+
+disp(['Energy in time domain: ', num2str(E1)])
+disp(['Energy in frequency domain: ', num2str(E2)])
+
+
+
+
+ 
+
+
+
